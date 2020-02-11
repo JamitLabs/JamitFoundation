5a809d234dfb2e6396991bc97ce3af8425b39db8
Simon Blum <Simon.Blum@jamitlabs.com>
2020-02-11 07:29:06 +0100

    Create sample xcode project and adjust project structure

---
+++ Examples/JamitFoundationExample/App/Sources/Main/ViewController.swift
@@ -0,0 +1,11 @@
+//  Copyright © 2020 Jamit Labs GmbH. All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
