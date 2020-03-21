22829c3d469e6e95626bce31b83071d0590bc613
Raoul Schwagmeier <r.schwagmeier@gmail.com>
2020-03-21 12:27:18 +0100

    Remove action callback

--- Sources/Component/Selectable/SelectableViewModel.swift
+++ Sources/Component/Selectable/SelectableViewModel.swift
@@ -4,22 +4,19 @@ public struct SelectableViewModel<Content: ViewModelProtocol>: ViewModelProtocol
     public typealias OnSelectionCallback = (Bool) -> Void
 
     public let content: Content
-    public let action: VoidCallback
     public let onSelected: OnSelectionCallback
 
     public init(
         content: Content = Self.default.content,
-        action: @escaping VoidCallback = Self.default.action,
         onSelected: @escaping OnSelectionCallback = Self.default.onSelected
     ) {
         self.content = content
-        self.action = action
         self.onSelected = onSelected
     }
 }
 
 extension SelectableViewModel {
     public static var `default`: SelectableViewModel<Content> {
-        .init()
+        .init(content: .default, onSelected: { _ in })
     }
 }
