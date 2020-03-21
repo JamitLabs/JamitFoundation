fdfcec5b63fff6dfee52457ec76cd7c20840c0d5
Raoul Schwagmeier <r.schwagmeier@gmail.com>
2020-03-21 11:10:50 +0100

    Implement SelectableView

---
+++ Sources/Component/Selectable/SelectableView.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+public final class SelectableView<ContenView: StatefulViewProtocol>: StatefulView<SelectableViewModel<ContenView.Model>> {
+    public var isSelected: Bool = false {
+        didSet {
+            model.onSelected(isSelected)
+        }
+    }
+
+    public private(set) lazy var view: ContenView = .instantiate()
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(view)
+        view.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        view.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        view.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        view.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+
+        isUserInteractionEnabled = true
+    }
+
+    public override func didChangeModel() {
+        super.didChangeModel()
+
+        view.model = model.content
+    }
+}
