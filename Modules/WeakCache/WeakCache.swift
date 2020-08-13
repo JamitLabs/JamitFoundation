bd7cc82bc6077978bcf6b5c1aa2c181cb5dcd39e
Jens Krug <jens.krug@jamitlabs.com>
2020-08-13 08:45:51 +0200

    Add developer documentation

--- Modules/WeakCache/WeakCache.swift
+++ Modules/WeakCache/WeakCache.swift
@@ -2,9 +2,23 @@
 
 import Foundation
 
+///
+/// `WeakCache` is a DataStructure based on `NSHashTable`. The reference counter of the stored elements is not
+/// increased by adding it to the `WeakCache`.
+/// If the element is deinitialized it is automatically removed from the cache.
+///
+/// Example:
+///
+/// ```swift
+/// let cache: WeakCache<UIViewController> = .init()
+///
+/// cache.append(someViewController)
+/// ```
+///
 public final class WeakCache<Element> {
     private let elements: NSHashTable<AnyObject>
 
+    /// All elements of the cache
     public var all: [Element] {
         return elements.allObjects.compactMap { $0 as? Element }
     }
@@ -13,18 +27,32 @@ public final class WeakCache<Element> {
         elements = NSHashTable<AnyObject>.weakObjects()
     }
 
+    /// Appends a new element to the cache
+    ///
+    /// - parameters:
+    ///     - element: The element to append
     public func append(_ element: Element) {
         elements.add(element as AnyObject)
     }
 
+    /// Removes an element from the cache
+    ///
+    /// - parameters:
+    ///     - element: The element to remove
     public func remove(_ element: Element) {
         elements.remove(element as AnyObject)
     }
 
+    /// Checks if the element is stored in the `WeakCache`
+    ///
+    /// - parameters:
+    ///     - element: The element to check
+    /// - returns: `true` if the given element is in the cache, `false` if not
     public func contains(_ element: Element) -> Bool {
         return elements.contains(element as AnyObject)
     }
 
+    /// Removes all elements from the cache
     public func removeAll() {
         return elements.removeAllObjects()
     }
