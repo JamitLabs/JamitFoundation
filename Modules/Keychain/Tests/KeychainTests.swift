51bceba20114b1d90530dacf7cfd1cf59ffd1d92
Raoul Schwagmeier <raoul.schwagmeier@jamitlabs.com>
2021-01-07 14:30:28 +0100

    Add tests for keychain

--- Modules/Keychain/Tests/KeychainTests.swift
+++ Modules/Keychain/Tests/KeychainTests.swift
@@ -3,4 +3,23 @@ import XCTest
 
 class KeychainTests: XCTestCase {
     let key: String = "jamitFoundation.userDefaults.Tests.sampleKey"
+    let mockKeychain: MockKeychain = .init()
+
+    override func tearDown() {
+        super.tearDown()
+
+        mockKeychain.clear()
+    }
+
+    func testSaveItem() {
+        var item: Secured<String?> = .init(key: key, keychain: mockKeychain as KeychainProtocol)
+        item.wrappedValue = "Hello World"
+
+        guard
+            let keychainData = mockKeychain.attributes[kSecValueData as String] as? Data,
+            let securedValue = try? JSONDecoder().decode(String.self, from: keychainData)
+        else { return XCTFail() }
+
+        XCTAssertEqual(securedValue, "Hello World")
+    }
 }
