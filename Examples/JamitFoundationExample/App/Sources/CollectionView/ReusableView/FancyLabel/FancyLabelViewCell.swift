a1bc526fceeeef053c0d6e606ef6f327b8cd0ebe
David Knothe <david.knothe@jamitlabs.net>
2021-01-26 12:27:03 +0100

    remove example

--- Examples/JamitFoundationExample/App/Sources/CollectionView/ReusableView/FancyLabel/FancyLabelViewCell.swift
+++
@@ -1,7 +0,0 @@
-//  Copyright © 2020 Jamit Labs GmbH. All rights reserved.
-//
-
-import Foundation
-import JamitFoundation
-
-final class FancyLabelViewCell: ContainerCollectionViewCell<FancyLabelView> {}
