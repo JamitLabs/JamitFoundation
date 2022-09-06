1fa42c33edf20419ff1426d23972dc3b7c5e8b01
Jonathan Gräser <jonathan.graeser@jamitlabs.com>
2022-09-06 17:05:14 +0200

    Add documentation

--- Modules/SwiftUISupport/StatefulSwiftUIView.swift
+++ Modules/SwiftUISupport/StatefulSwiftUIView.swift
@@ -1,12 +1,17 @@
-import Foundation
 import JamitFoundation
 import UIKit
 import SwiftUI
 
 @available(iOS 13.0, *)
-public struct StatefulSwiftUIView<ContentView: StatefulViewProtocol>: UIViewRepresentable {
+/// Takes a stateful view as a generic type and wraps it into a `UIViewRepresentable`.
+/// The stateful views model can be changed by changing the `model` property.
+public struct StatefulViewRepresentable<ContentView: StatefulViewProtocol>: UIViewRepresentable {
+    /// The current state of the view.
     @Binding private(set) var model: ContentView.Model
 
+    /// Initializes a `StatefulViewRepresentable`
+    ///
+    /// - Parameter model: The model associated with the stateful view
     public init(
         model: Binding<ContentView.Model> = .constant(.default)
     ) {
