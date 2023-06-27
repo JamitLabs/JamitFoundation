3ca33058bdae3f19f80aae5d60e5ebd5e7a695c1
Philip Fritz <philip.fritz@jamitlabs.com>
2023-06-27 14:49:14 +0200

    Add scrollToPage method and verticalScrollEnabled bool

--- Modules/PageView/PageViewModel.swift
+++ Modules/PageView/PageViewModel.swift
@@ -25,19 +25,25 @@ public struct PageViewModel<Content: ViewModelProtocol>: ViewModelProtocol {
     /// The callback for page index changes.
     public let onPageIndexChanged: PageIndexChangeCallback
 
+    /// Wheter the PageView is vertically scrollable or not.
+    public let verticalScrollEnabled: Bool
+
     /// The default initializer of `PageViewModel`.
-    /// 
+    ///
     /// - Parameter axis: The scroll axis for the paging behaviour.
     /// - Parameter pages: The paginated content view models.
     /// - Parameter onPageIndexChanged: The content view models for the paginated content views.
+    /// - Parameter verticalScrollEnabled: Whether the pageview is scrolling verticaly or not.
     public init(
         axis: Axis = PageViewModel.default.axis,
         pages: [Content] = PageViewModel.default.pages,
-        onPageIndexChanged: @escaping PageIndexChangeCallback = PageViewModel.default.onPageIndexChanged
+        onPageIndexChanged: @escaping PageIndexChangeCallback = PageViewModel.default.onPageIndexChanged,
+        verticalScrollEnbaled: Bool = CustomPageViewModel.default.verticalScrollEnabled
     ) {
         self.axis = axis
         self.pages = pages
         self.onPageIndexChanged = onPageIndexChanged
+        self.verticalScrollEnabled = verticalScrollEnbaled
     }
 }
 
@@ -47,7 +53,8 @@ extension PageViewModel {
         return .init(
             axis: .horizontal,
             pages: [],
-            onPageIndexChanged: { _ in }
+            onPageIndexChanged: { _ in },
+            verticalScrollEnbaled: true,
         )
     }
 }
