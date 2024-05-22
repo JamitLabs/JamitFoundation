75cfdcb4ba23b37779b4d5347b851f86d12c8c56
Jonas Frey <jonas.frey@jamitlabs.com>
2024-05-22 10:35:56 +0200

    Move KeychainError to separate file

--- Modules/Keychain/Sources/Secured.swift
+++ Modules/Keychain/Sources/Secured.swift
@@ -10,61 +10,9 @@ import OSLog
 /// ```
 @propertyWrapper
 public struct Secured<Value: Codable> {
-    private enum KeychainError: Error, LocalizedError {
-        case itemNotFound
-        case accessError(status: String)
-        case decodingError(error: Error)
-        case encodingError(error: Error)
-        case saveItemToKeychain(status: String)
-        case deleteItem
-
-        var errorDescription: String? {
-            switch self {
-            case .itemNotFound: return "The requested item could not be found in the keychain"
-            case .accessError: return "The keychain item could not be accessed"
-            case .decodingError: return "The retrieved keychain data could not be decoded"
-            case .encodingError: return "The item could not be encoded"
-            case .saveItemToKeychain: return "The item could not be saved to the keychain"
-            case .deleteItem: return "The item could not be removed from the keychain"
-            }
-        }
-
-        var failureReason: String? {
-            switch self {
-            case let .accessError(status: error): return error
-            case let .decodingError(error: error): return error.localizedDescription
-            case let .encodingError(error: error): return error.localizedDescription
-            case let .saveItemToKeychain(status: error): return error
-            default: return nil
-            }
-        }
-
-        var recoverySuggestion: String? {
-            switch self {
-            case .itemNotFound: return "Please make sure that the item exists in the keychain before you try to access it"
-            case .deleteItem: return "Please make sure that the item exists in the keychain before you try to delete it"
-            default: return nil
-            }
-        }
-
-        func description(for key: String) -> String {
-            var description: String = .init(format: "A keychain error associated with the key: %@ occurred", key)
-
-            if let errorDescription = self.errorDescription {
-                description.append(String(format: "\nDescription: %@", errorDescription))
-            }
-
-            if let reason = failureReason {
-                description.append(String(format: "\nReason: %@", reason))
-            }
-
-            if let recoverySuggestion = recoverySuggestion {
-                description.append(String(format: "\nRecovery Suggestion: %@", recoverySuggestion))
-            }
-
-            return description
-        }
-    }
+    private let key: String
+    private let keychain: KeychainProtocol
+    private let accessGroup: String?
 
     /// The wrapped value of the property value used to directly access the value
     public var wrappedValue: Value? {
@@ -89,10 +37,6 @@ public struct Secured<Value: Codable> {
         }
         return query
     }
-
-    private let key: String
-    private let keychain: KeychainProtocol
-    private let accessGroup: String?
 
     /// The default initializer for `Secured`
     ///
